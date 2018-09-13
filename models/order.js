b3c127cc257603f8f3a66a2933d2d096eb4afa14
angelikasalvadora <angelika.salvadora@gmail.com>
2018-09-13 23:31:53 +0800

    Module5

--- models/order.js
+++ models/order.js
@@ -1,6 +1,6 @@
 var Orders = {
   olist: (client, filter, callback) => {
-    const orderListQuery = `SELECT orders.id,first_name,last_name,order_date,street,municipality,zipcode,product_name,price,province,quantity,price*quantity as total FROM orders INNER JOIN products ON orders.product_id = products.id INNER JOIN customers on orders.customer_id = customers.id
+    const orderListQuery = `SELECT orders.id,first_name,last_name,order_date,street,municipality,zipcode,product_name,price,province,quantity,price*quantity as total FROM orders INNER JOIN products ON orders.product_id = products.id INNER JOIN customers on orders.customer_id = customers.id ORDER BY order_date DESC
       `;
     client.query(orderListQuery, (req, data) => {
       console.log(data.rows);
